0de8a2a26c39ed51702ceaa683b9ecdcd103bb82
Mayank Rathi <mayankrathi72@gmail.com>
2025-04-27 16:35:56 +0530

    Upload backend code with a small update

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app = FastAPI()
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://reddit-analyzer-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
